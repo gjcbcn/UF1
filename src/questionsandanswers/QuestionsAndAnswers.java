12d9482f5bdd2aff6f3bf2afa0f2c7f356a6210f
Gerard <Gerard@172.16.0.175>
2013-12-13 17:12:23 +0100

    La primera vez

---
+++ src/questionsandanswers/QuestionsAndAnswers.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package questionsandanswers;
+
+/**
+ *
+ * @author Gerard
+ */
+public class QuestionsAndAnswers {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
